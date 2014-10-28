c3f5d62dfd5456fef5e0091ec7ed6421fdeb4fcc
xiaojf <294825811@qq.com>
2014-10-28 23:29:55 +0800

    加入font-awesome-4.2.0 字体库

--- src/main/webapp/app/view/main/Main.js
+++ src/main/webapp/app/view/main/Main.js
@@ -1,47 +1,58 @@
 /**
- * This class is the main view for the application. It is specified in app.js as the
- * "autoCreateViewport" property. That setting automatically applies the "viewport"
- * plugin to promote that instance of this class to the body element.
- *
- * TODO - Replace this content of this view to suite the needs of your application.
+ * This class is the main view for the application. It is specified in app.js as
+ * the "autoCreateViewport" property. That setting automatically applies the
+ * "viewport" plugin to promote that instance of this class to the body element.
+ * 
+ * TODO - Replace this content of this view to suite the needs of your
+ * application.
  */
 Ext.define('app.view.main.Main', {
-    extend: 'Ext.container.Container',
-    requires: [
-        'app.view.main.MainController',
-        'app.view.main.MainModel'
-    ],
+	extend : 'Ext.container.Container',
+	requires : ['app.view.main.MainController', 'app.view.main.MainModel'],
 
-    xtype: 'app-main',
-    
-    controller: 'main',
-    viewModel: {
-        type: 'main'
-    },
+	initComponent : function() {
+		Ext.setGlyphFontFamily('FontAwesome'); // 设置图标字体文件，只有设置了以后才能用glyph属性
+		this.callParent();
+	},
 
-    layout: {
-        type: 'border'
-    },
+	uses : ['app.view.main.region.Top', 'app.view.main.region.Bottom'],
 
-    items: [{
-        xtype: 'panel',
-        bind: {
-            title: '{name}'
-        },
-        region: 'west',
-        html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
-        width: 250,
-        split: true,
-        tbar: [{
-            text: 'Button',
-            handler: 'onClickButton'
-        }]
-    },{
-        region: 'center',
-        xtype: 'tabpanel',
-        items:[{
-            title: 'Tab 1',
-            html: '<h2>Content appropriate for the current navigation.</h2>'
-        }]
-    }]
+	xtype : 'app-main',
+
+	controller : 'main',
+	viewModel : {
+		type : 'main'
+	},
+
+	layout : {
+		type : 'border'
+	},
+
+	items : [{
+				region : 'north',
+				xtype : 'maintop'
+			}, {
+				region : 'south',
+				xtype : 'mainbottom'
+			}, {
+				xtype : 'panel',
+				bind : {
+					title : '{name}'
+				},
+				region : 'west',
+				html : '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
+				width : 250,
+				split : true,
+				tbar : [{
+							text : 'Button',
+							handler : 'onClickButton'
+						}]
+			}, {
+				region : 'center',
+				xtype : 'tabpanel',
+				items : [{
+					title : 'Tab 1',
+					html : '<h2>Content appropriate for the current navigation.</h2>'
+				}]
+			}]
 });
