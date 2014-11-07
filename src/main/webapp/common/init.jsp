250c45a43d86e81f27dd99344a2ca27ce44b6199
xiaojf <294825811@qq.com>
2014-11-07 20:35:13 +0800

    添加公共 util.js , init.jsp文件

---
+++ src/main/webapp/common/init.jsp
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="js/easyui1.4.1/themes/bootstrap/easyui.css">
+<link rel="stylesheet" type="text/css" href="js/easyui1.4.1/themes/icon.css">
+<script type="text/javascript" src="js/easyui1.4.1/locale/easyui-lang-zh_CN.js"></script>
+<script type="text/javascript" src="js/easyui1.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/easyui1.4.1/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="common/util.js"></script>
---
+++ src/main/webapp/common/util.js
@@ -0,0 +1,63 @@
+/**
+ * 验证重复密码
+ * @author sean294825811@sina.com
+ */
+$.extend($.fn.validatebox.defaults.rules, {   
+    rePassword: {   
+        validator: function(value, param){   
+            return value == $(param[0]).val();   
+        },   
+        message: '密码和重复密码不一致'  
+    }   
+});
+/**
+ * 表单序列化, 需要传入一个form的jquery对象
+ */
+serializeObject = function(form) {
+	var o = {};
+	$.each(form.serializeArray(), function(index) {
+		if (o[this['name']]) {
+			o[this['name']] = o[this['name']] + "," + this['value'];
+		} else {
+			o[this['name']] = this['value'];
+		}
+	});
+	return o;
+};
+/**
+ * 
+ * @requires jQuery,EasyUI
+ * 
+ * 防止panel/window/dialog组件超出浏览器边界
+ * @param left
+ * @param top
+ */
+var easyuiPanelOnMove = function(left, top) {
+	var l = left;
+	var t = top;
+	if (l < 1) {
+		l = 1;
+	}
+	if (t < 1) {
+		t = 1;
+	}
+	var width = parseInt($(this).parent().css('width')) + 14;
+	var height = parseInt($(this).parent().css('height')) + 14;
+	var right = l + width;
+	var buttom = t + height;
+	var browserWidth = $(window).width();
+	var browserHeight = $(window).height();
+	if (right > browserWidth) {
+		l = browserWidth - width;
+	}
+	if (buttom > browserHeight) {
+		t = browserHeight - height;
+	}
+	$(this).parent().css({/* 修正面板位置 */
+		left : l,
+		top : t
+	});
+};
+$.fn.dialog.defaults.onMove = easyuiPanelOnMove;
+$.fn.window.defaults.onMove = easyuiPanelOnMove;
+$.fn.panel.defaults.onMove = easyuiPanelOnMove;
